d6a0ca821a828eb0a69c10d4079dc51e9e7c7fe7
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-07-06 22:36:27 +0200

    add notActivated class

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -1,52 +1,54 @@
 import FullList from "../models/FullList";
 
 interface DOMList {
-    ul: HTMLUListElement,
-    clear(): void,
-    render(fullList: FullList): void
+  ul: HTMLUListElement;
+  clear(): void;
+  render(fullList: FullList): void;
 }
 
 export default class ListTemplate implements DOMList {
-    ul: HTMLUListElement
+  ul: HTMLUListElement;
 
-    static instance: ListTemplate = new ListTemplate()
+  static instance: ListTemplate = new ListTemplate();
 
-    private constructor() {
-        this.ul = document.getElementById("listItems") as HTMLUListElement
-    }
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement;
+  }
 
-    clear(): void {
-        this.ul.innerHTML = ""
-    }
+  clear(): void {
+    this.ul.innerHTML = "";
+  }
 
-    render(fullList: FullList): void {
-        this.clear()
+  render(fullList: FullList): void {
+    this.clear();
 
-        fullList.list.map((item) => {
-            const li = document.createElement("li") as HTMLLIElement
-            li.className = "item"
-            //li.id = item.id
-            if (item.active === true) {
-                li.className = "activated"
-            }
+    fullList.list.map((item) => {
+      const li = document.createElement("li") as HTMLLIElement;
+      li.className = "item";
+      //li.id = item.id
+      if (item.active !== true) {
+        li.className = "notActivated";
+      }
 
-            const poster = document.createElement("img") as HTMLImageElement
-            poster.setAttribute("src", item.title)
-            li.appendChild(poster)
+      const poster = document.createElement("img") as HTMLImageElement;
+      poster.setAttribute("src", item.title);
+      li.appendChild(poster);
 
-            const title = document.createElement("h3") as HTMLHeadElement
-            title.innerText = item.title
-            li.appendChild(title)
+      const title = document.createElement("h3") as HTMLHeadElement;
+      title.innerText = item.title;
+      li.appendChild(title);
 
-            const yearOfRelease = document.createElement("p") as HTMLHeadElement
-            title.innerText = item.yearOfRelease
-            li.appendChild(yearOfRelease)
+      const yearOfRelease = document.createElement("p") as HTMLHeadElement;
+      title.innerText = item.yearOfRelease;
+      yearOfRelease.className = "yearOfRelease";
+      li.appendChild(yearOfRelease);
 
-            const description = document.createElement("p") as HTMLHeadElement
-            description.innerText = item.description
-            li.appendChild(description)
+      const description = document.createElement("p") as HTMLHeadElement;
+      description.className = "description";
+      description.innerText = item.description;
+      li.appendChild(description);
 
-            this.ul.appendChild(li)
-        })
-    }
+      this.ul.appendChild(li);
+    });
+  }
 }
