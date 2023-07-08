37c10d591d571d525e09c437761aa3e0b1ae5162
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-07-08 22:11:08 +0200

    done the search

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import ListItem from "./models/ListItem";
 import ListTemplate from "./templates/ListTemplate";
 
 let movie: string = "";
+let year: number;
+let url: string;
 
 const initApp = (): void => {
   const fullList = FullList.instance;
@@ -15,39 +17,40 @@ const initApp = (): void => {
     const entryInput = document.getElementById(
       "entryInput"
     ) as HTMLInputElement;
-    const newEntry = entryInput.value.trim();
-    console.log(newEntry.length);
+    movie = entryInput.value.trim();
 
-    const select = document.getElementById(
-      "selectTypeOfSearch"
-    ) as HTMLSelectElement;
+    const yearInput = document.getElementById("yearInput") as HTMLInputElement;
+    const newYearEntry = yearInput.value.trim();
 
-    if (!newEntry.length) {
-      alert("Enter a title or year of release");
-    }
-
-    if (select.value === "1") {
-      movie = newEntry;
+    if (!movie.length) {
+      alert("Enter your title");
     } else {
-      alert("Wybierz typ wyszukiwania");
+      if (!newYearEntry.length) {
+
+        url = `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US`;
+
+      } else if (newYearEntry.length !== 4) {alert("Enter valid year of release")} else {
+        year = Number(newYearEntry)
+        url = `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&primary_release_year=${year}&fbclid=IwAR3ocwlIGvQtZ2hKU5y7bgOz2r5raO3Hj4Z-_uZ7IJkFq465l-NZLxTOFjM`
+      }
+
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzYxZGU0YzM4NTcxMGFmYjM1NmFiZjhkMDE5ZTZmNSIsInN1YiI6IjY0YTA3ZGFmOGMwYTQ4MDEwMTc2N2M5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QX5f2XFVOeg7um0kUMfuFcq-GIxINGyWCoWz7JQ6juA",
+        },
+      };
+
+      fetch(
+        url,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => console.log(response.results))
+        .catch((err) => console.error(err));
     }
-
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzYxZGU0YzM4NTcxMGFmYjM1NmFiZjhkMDE5ZTZmNSIsInN1YiI6IjY0YTA3ZGFmOGMwYTQ4MDEwMTc2N2M5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QX5f2XFVOeg7um0kUMfuFcq-GIxINGyWCoWz7JQ6juA",
-      },
-    };
-
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => console.log(response.results))
-      .catch((err) => console.error(err));
   });
 };
 
